b615dd2b7aefd2e712d6d30c60a6e20b5f96f6db
M-MuneebAlam <mmuneeb.alam09@gmail.com>
2025-08-25 13:24:12 +0500

    Configure redux provider,  Install and configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth.js'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  
-  return (
-    <>
-      <h1>A Blog App with Appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+  })
+  .finally(() => setLoading(false))
+  }, [dispatch])
+
+  return !loading ? (
+    <div className='min-h-screen flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        {/* TODO  <Outlet /> */}
+        <Footer />
+        </main>  
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
