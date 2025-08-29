b17aa56070b849df39068b2f5abab5f20c6e784b
M-MuneebAlam <mmuneeb.alam09@gmail.com>
2025-08-29 12:43:58 +0500

    Update logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-function Logo({ width = "100px" }) {
+function Logo({ width = "120px" }) {
+  const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    width,
+    fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif",
+    fontWeight: '600',
+    fontSize: '1.2rem',
+    color: '#111',
+  };
+
+  const accentStyle = {
+    width: '8px',
+    height: '8px',
+    borderRadius: '50%',
+    backgroundColor: '#ff5a5f',
+    marginRight: '8px',
+  };
+
   return (
-    <div style={{ width }}>
-      Logo
+    <div style={containerStyle}>
+      <div style={accentStyle}></div>
+      <span>MyBlog</span>
     </div>
-  )
+  );
 }
 
+export default Logo;
 
-export default Logo
+
+
+
+// import React from 'react'
+
+// function Logo({ width = "100px" }) {
+//   return (
+//     <div style={{ width }}>
+//       Logo
+//     </div>
+//   )
+// }
+
+
+// export default Logo
+
+
